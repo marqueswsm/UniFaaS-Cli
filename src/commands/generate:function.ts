cca702eea87de9f53ea54b1a5be5bb38fa0d5b6d
wag <wagnermarques@Pro.local>
2020-01-02 21:26:35 -0300

    :recycle: generate docker function

--- src/commands/generate:function.ts
+++ src/commands/generate:function.ts
@@ -1,50 +1,69 @@
+import * as R from 'ramda';
+
+const docker = async (toolbox) =>  {
+    const {
+        parameters: { options },
+        template,
+        print: { success, error },
+    } = toolbox;
+
+    const name = R.prop('name', options);
+
+    if (!name) {
+        error('Function name must be specified');
+        return
+    }
+
+    await template.generate({
+        template: 'docker/template.ts.ejs',
+        target: `${name}/index.js`,
+    });
+
+    await template.generate({
+        template: 'docker/conf.json.ejs',
+        target: `${name}/function.json`,
+    });
+
+    await template.generate({
+        template: 'docker/dockerfile.ejs',
+        target: `Dockerfile`,
+    });
+
+    await template.generate({
+        template: 'docker/host.json.ejs',
+        target: 'host.json',
+    });
+
+    await template.generate({
+        template: 'docker/local.settings.json.ejs',
+        target: 'local.settings.json',
+    });
+
+    await template.generate({
+        template: 'docker/package.json.ejs',
+        target: 'package.json'
+    });
+
+    success(`Generated a function called ${name}`)
+};
+
 module.exports = {
     name: 'generate:function',
     description: 'Create a template function inside the current folder',
     run: async toolbox => {
-        const {
-            parameters,
-            template,
-            print: { success, error },
-        } = toolbox;
+        const options = R.path(['parameters', 'options'], toolbox);
+        const print   = R.prop('print', toolbox); 
 
-        const name = parameters.first;
-
-        if (!name) {
-            error('Function name must be specified');
-            return
+        if (!options.virt || !options.name) {
+            print.info('Usage: uni-faas generate:function --virt [DOCKER | UNIK] --name [OPTION]');
         }
 
-        await template.generate({
-            template: 'template.ts.ejs',
-            target: `${name}/index.js`,
-        });
+        if (options.virt === 'docker') {
+            await docker(toolbox);
+        }
 
-        await template.generate({
-            template: 'conf.json.ejs',
-            target: `${name}/function.json`,
-        });
-
-        await template.generate({
-            template: 'dockerfile.ejs',
-            target: `Dockerfile`,
-        })
-
-        await template.generate({
-            template: 'host.json.ejs',
-            target: 'host.json',
-        })
-
-        await template.generate({
-            template: 'local.settings.json.ejs',
-            target: 'local.settings.json',
-        })
-
-        await template.generate({
-            template: 'package.json.ejs',
-            target: 'package.json'
-        })
-
-        success(`Generated a function called ${parameters.first}`)
+        if (options.virt === 'unik') {
+            console.log('Not yet');
+        }
     }
 }
