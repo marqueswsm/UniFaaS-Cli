d03d9bf77b942fd64cd53545aec992b78c6b58ca
Wagner Marques <wagnermarques@Wagners-MacBook-Pro.local>
2020-01-20 22:22:57 -0300

    change call function to call the capstan run first

--- src/commands/call.ts
+++ src/commands/call.ts
@@ -64,7 +64,7 @@ module.exports = {
     }
 
     if (options.base === 'osv') {
-      await sh(`capstan start ${options.name}`);
+      await sh(`capstan run -f 3000:3000`);
 
       axios.interceptors.response.use((response) => {
         return response;
